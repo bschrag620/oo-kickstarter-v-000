8aae61e49460168896d92a99d1bae1dc417b0532
Brad Schrag <brad.schrag@gmail.com>
2018-11-20 21:29:09 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,6 +9,8 @@ class Backer
 
   def back_project(project)
     self.backed_projects << project
-    project.add_backer(self)
+    if !project.backers.include?(self)
+      project.add_backer(self)
+    end
   end
 end
