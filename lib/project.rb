c5f26c5017897307403075e327029f2bf47db0c0
Brad Schrag <brad.schrag@gmail.com>
2018-11-20 21:26:10 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,14 @@
 class Project
 
-  attr_accessor :title
+  attr_accessor :title, :backer
 
   def initialize(title)
     @title = title
   end
+
+  def add_backer(backer)
+    self.backer = backer
+  end
+  
 
 end
